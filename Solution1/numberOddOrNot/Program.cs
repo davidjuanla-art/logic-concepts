d19a173313ca9c6f373bc137da6d5b12729b28ff
Acer 23 <Acer 23@WINDOUS_32>
2026-08-08 14:22:21 -0500

    Por mejorar

---
+++ Solution1/numberOddOrNot/Program.cs
@@ -0,0 +1,16 @@
+﻿do
+{
+    Console.WriteLine("Ingrese un nùmero o control + c para salir");
+
+var numero = Console.ReadLine();
+var numeroInt = int.Parse(numero!);
+
+
+    if (numeroInt % 2 == 0) {
+        Console.WriteLine($"el nùmero { numeroInt} es par");
+    } else {
+        Console.WriteLine($"el nùmero  {numeroInt} es impar");
+
+    }
+
+} while (true) ;
