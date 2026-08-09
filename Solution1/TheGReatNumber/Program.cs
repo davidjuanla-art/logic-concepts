16ea165dfb4d689a1acb56f68e6402ebd2060ce0
Acer 23 <Acer 23@WINDOUS_32>
2026-08-09 16:48:26 -0500

    .

---
+++ Solution1/TheGReatNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using Shared;
+
+var  x = ConsoleExtension.GetInt("Ingrese primer nùmero: ");
+var y = ConsoleExtension.GetInt("Ingrese segundo nùmero: ");
+var z = ConsoleExtension.GetInt("Ingrese tercer nùmero: ");
+
+
+do
+{
+    if (x >= y && x >= z)
+    {
+        Console.WriteLine($"El nùmero mayor es: {x}");
+    }
+    else if (y >= x && y >= z)
+    {
+        Console.WriteLine($"El nùmero mayor es: {y}");
+    }
+    else
+            {
+        Console.WriteLine($"El nùmero mayor es: {z}");
+    }
+} while (true);
+
