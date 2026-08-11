52f379b78fe1a4ed4be97436132c14a29509524b
Acer 23 <Acer 23@WINDOUS_32>
2026-08-10 20:45:49 -0500

    TheGreatsNumbers

---
+++ LogicConcepts/SortingOfThreeNumbers/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using Shared;
+
+
+var x = ConsoleExtension.GetInt("Ingrese primer nùmero: ");
+var y = ConsoleExtension.GetInt("Ingrese segundo nùmero: ");
+var z = ConsoleExtension.GetInt("Ingrese tercer nùmero: ");
+
+int Mayor = 0;
+int Medio = 0;
+int Menor = 0;
+
+if (x >= y && x >= z)
+{
+    Mayor = x;
+    if (y >= z)
+    {
+        Medio = y;
+        Menor = z;
+        Console.WriteLine($"El numero mayor es: {Mayor}, el del medio es: {Medio} y el menor es: {Menor}");
+    }
+    else if (y >= x && y >= z)
+    {
+        Mayor = y;
+
+        if (z >= x) 
+        {
+        Medio = z;
+        Menor = x;
+            Console.WriteLine($"El numero mayor es: {Mayor}, el del medio es: {Medio} y el menor es: {Menor}");
+        }
+    }
+    else
+    {
+        Mayor = z;
+
+        if (y >= x)
+        {
+            Medio = y;
+            Menor = x;
+        }
+        else
+        {
+            Medio = x;
+            Menor = y;
+        }
+        Console.WriteLine($"El numero mayor es: {Mayor}, el del medio es: {Medio} y el menor es: {Menor}");
+    }
+    answer = ConsoleExtension.GetValidOptions("¿Desea repetir? (s/n): ", options);
+
+} while (answer != null && answer.Equals("s", StringComparison.CurrentCultureIgnoreCase)) ;
+}
+   
--- LogicConcepts/TheGreatsNumber/Program.cs
+++ LogicConcepts/TheGreatsNumber/Program.cs
@@ -1,21 +1,31 @@
 ﻿
 using Shared;
 
-var x = ConsoleExtension.GetInt("Ingrese primer nùmero: ");
-var y = ConsoleExtension.GetInt("Ingrese segundo nùmero: ");
-var z = ConsoleExtension.GetInt("Ingrese tercer nùmero: ");
+var answer = String.Empty;
+var options = new List<string> { "s", "n" };
+
+do 
+{ 
+
+    var x = ConsoleExtension.GetInt("Ingrese primer nùmero: ");
+    var y = ConsoleExtension.GetInt("Ingrese segundo nùmero: ");
+    var z = ConsoleExtension.GetInt("Ingrese tercer nùmero: ");
 
 
+    if (x >= y && x >= z)
+    {
+        Console.WriteLine($"El nùmero mayor es: {x}");
+    }
+    else if (y >= x && y >= z)
+    {
+        Console.WriteLine($"El nùmero mayor es: {y}");
+    }
+    else
+    {
+        Console.WriteLine($"El nùmero mayor es: {z}");
+    }
 
-if (x >= y && x >= z)
-{
-    Console.WriteLine($"El nùmero mayor es: {x}");
-}
-else if (y >= x && y >= z)
-{
-    Console.WriteLine($"El nùmero mayor es: {y}");
-}
-else
-{
-    Console.WriteLine($"El nùmero mayor es: {z}");
-}
+    
+   answer = ConsoleExtension.GetValidOptions("¿Desea repetir? (s/n): ", options);
+
+} while (answer != null && answer.Equals("s", StringComparison.CurrentCultureIgnoreCase)) ;
