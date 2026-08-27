029da18479550e96fdba5cb5bc4d64edbd3c1726
Acer 23 <Acer 23@WINDOUS_32>
2026-08-26 19:06:32 -0500

    Taller 2.

---
+++ Taller 2/Backend/Circle.cs
@@ -0,0 +1,28 @@
+﻿namespace Backend_;
+
+    public class Circle 
+
+    {
+        // fields
+
+        private double _r;
+
+    //constructor
+   
+    public Circle(double r)
+    {
+        this._r = r;
+    }
+
+    //properties
+
+    public double R { get; set; } 
+
+    //methods 
+
+
+
+
+}
+
+
---
+++ Taller 2/Backend/GeometricFigure.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace Backend_;
+
+public abstract class GeometricFigure
+{
+    // Constructors
+    protected GeometricFigure(String name)
+    {
+        this.name = name;
+    }
+
+
+    //properties
+    public String name { get; set; } = null!;
+
+
+
+    //method
+
+    public abstract double GetArea();
+
+    public abstract double GetPerimeter();
+
+    public override string ToString()
+    {
+        return $"{name}/t => Area. . . . . :/t{GetArea}/t perimeter:/t {GetPerimeter}";
+
+
+
+    }
+}
+
