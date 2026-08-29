b4a2300242c0195d4afcf506eb9b66882ec28bbf
Acer 23 <Acer 23@WINDOUS_32>
2026-08-28 20:29:08 -0500

    Creation of the backend after the first time it was saved in the wrong GitHub folder.

---
+++ Taller 2 POO/GeometricFigures.Backend/Class1.cs
@@ -0,0 +1,30 @@
+﻿namespace GeometricFigures.Backend;
+public abstract class GeometricFigure
+{
+    // Constructors
+    protected GeometricFigure(String name)
+    {
+        this.name = name;
+    }
+
+
+    //properties
+    public String name { get; set; } = null!;
+
+
+
+    //method
+
+    public abstract double GetArea();
+
+    public abstract double GetPerimeter();
+
+    public override string ToString()
+    {
+        return $"{name}\t        => Area.....: {GetArea():f5}\t Perimeter:    {GetPerimeter():f5}";
+
+
+
+
+    }
+}
